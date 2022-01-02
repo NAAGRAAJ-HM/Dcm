c83cf1e4f5133357931186cc35ee0c51e4cea9b3
root <nagaraja.hm.0001@gmail.com>
2022-01-02 22:25:35 +0100

    Update: submodule Dcm

---
+++ LATEST/Dcm.cpp
@@ -0,0 +1,7 @@
+#include "Dcm.h"
+
+class_Dcm Dcm;
+
+void class_Dcm::InitFunction(void){
+}
+
---
+++ LATEST/Dcm.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class class_Dcm{
+   public:
+      void InitFunction(void);
+};
+
+extern class_Dcm Dcm;
+
