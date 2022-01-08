8ee77f026f1ecea1fa55bb9f6db51f62ba9c7d18
root <nagaraja.hm.0001@gmail.com>
2022-01-08 22:35:29 +0100

    Update: submodule Dcm

--- LATEST/Dcm.cpp
+++ LATEST/Dcm.cpp
@@ -5,3 +5,84 @@ class_Dcm Dcm;
 void class_Dcm::InitFunction(void){
 }
 
+void class_Dcm::GetVersionInfo(void){
+}
+
+void class_Dcm::DemTriggerOnDtcStatus(void){
+}
+
+void class_Dcm::GetVin(void){
+}
+
+void class_Dcm::GetSecurityLevel(void){
+}
+
+void class_Dcm::GetSesCtrlType(void){
+}
+
+void class_Dcm::GetActiveProtocol(void){
+}
+
+void class_Dcm::ResetToDefaultSession(void){
+}
+
+void class_Dcm::TriggerOnEvent(void){
+}
+
+void class_Dcm::SetActiveDiagnostic(void){
+}
+
+void class_Dcm::StartOfReception(void){
+}
+
+void class_Dcm::CopyRxData(void){
+}
+
+void class_Dcm::TpRxIndication(void){
+}
+
+void class_Dcm::CopyTxData(void){
+}
+
+void class_Dcm::TpTxConfirmation(void){
+}
+
+void class_Dcm::TxConfirmation(void){
+}
+
+void class_Dcm::ComM_NoComModeEntered(void){
+}
+
+void class_Dcm::ComM_SilentComModeEntered(void){
+}
+
+void class_Dcm::ComM_FullComModeEntered(void){
+}
+
+void class_Dcm::CallOut_ReadMemory(void){
+}
+
+void class_Dcm::CallOut_WriteMemory(void){
+}
+
+void class_Dcm::CallOut_SetProgConditions(void){
+}
+
+void class_Dcm::CallOut_GetProgConditions(void){
+}
+
+void class_Dcm::CallOut_ProcessRequestTransferExit(void){
+}
+
+void class_Dcm::CallOut_ProcessRequestUpload(void){
+}
+
+void class_Dcm::CallOut_ProcessRequestDownload(void){
+}
+
+void class_Dcm::CallOut_ProcessRequestFileTransfer(void){
+}
+
+void class_Dcm::MainFunction(void){
+}
+
--- LATEST/Dcm.h
+++ LATEST/Dcm.h
@@ -2,7 +2,34 @@
 
 class class_Dcm{
    public:
-      void InitFunction(void);
+      void InitFunction                       (void);
+      void GetVersionInfo                     (void);
+      void DemTriggerOnDtcStatus              (void);
+      void GetVin                             (void);
+      void GetSecurityLevel                   (void);
+      void GetSesCtrlType                     (void);
+      void GetActiveProtocol                  (void);
+      void ResetToDefaultSession              (void);
+      void TriggerOnEvent                     (void);
+      void SetActiveDiagnostic                (void);
+      void StartOfReception                   (void);
+      void CopyRxData                         (void);
+      void TpRxIndication                     (void);
+      void CopyTxData                         (void);
+      void TpTxConfirmation                   (void);
+      void TxConfirmation                     (void);
+      void ComM_NoComModeEntered              (void);
+      void ComM_SilentComModeEntered          (void);
+      void ComM_FullComModeEntered            (void);
+      void CallOut_ReadMemory                 (void);
+      void CallOut_WriteMemory                (void);
+      void CallOut_SetProgConditions          (void);
+      void CallOut_GetProgConditions          (void);
+      void CallOut_ProcessRequestTransferExit (void);
+      void CallOut_ProcessRequestUpload       (void);
+      void CallOut_ProcessRequestDownload     (void);
+      void CallOut_ProcessRequestFileTransfer (void);
+      void MainFunction                       (void);
 };
 
 extern class_Dcm Dcm;
