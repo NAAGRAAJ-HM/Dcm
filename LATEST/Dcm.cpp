d2c1eb39990f62f5397b4fad1e855f6b635f05ba
root <nagaraja.hm.0001@gmail.com>
2022-02-10 12:40:08 +0100

    Update: submodule Dcm

--- LATEST/Dcm.cpp
+++ LATEST/Dcm.cpp
@@ -6,106 +6,121 @@
 /*****************************************************/
 /* #INCLUDES                                         */
 /*****************************************************/
-#include "Dcm.h"
+#include "module.h"
 
-#include "Dcm_EcuM.h"
-#include "Dcm_SchM.h"
+#include "Dcm_Unused.h"
+
+/*****************************************************/
+/* #DEFINES                                          */
+/*****************************************************/
+
+/*****************************************************/
+/* MACROS                                            */
+/*****************************************************/
+
+/*****************************************************/
+/* TYPEDEFS                                          */
+/*****************************************************/
+class module_Dcm : public class_module{
+   public:
+      FUNC(void, DCM_CODE) InitFunction   (void);
+      FUNC(void, DCM_CODE) DeInitFunction (void);
+      FUNC(void, DCM_CODE) MainFunction   (void);
+};
 
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-interface_Dcm_EcuM_Init Dcm_EcuM_Init;
-interface_Dcm_SchM_Main Dcm_SchM_Main;
-class_Dcm Dcm;
+module_Dcm Dcm;
 
-interface_EcuM_Init_Client *EcuM_Init_Client_ptr_Dcm = &Dcm_EcuM_Init;
-interface_SchM_Main_Client *SchM_Main_Client_ptr_Dcm = &Dcm_SchM_Main;
+interface_EcuM_Client *EcuM_Client_ptr_Dcm = &Dcm;
+interface_SchM_Client *SchM_Client_ptr_Dcm = &Dcm;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-FUNC(void, DCM_CODE) interface_Dcm_EcuM_Init::InitFunction(void){
+FUNC(void, DCM_CODE) module_Dcm::InitFunction(void){
 }
 
-FUNC(void, DCM_CODE) interface_Dcm_SchM_Main::MainFunction(void){
+FUNC(void, DCM_CODE) module_Dcm::MainFunction(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::GetVersionInfo(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::GetVersionInfo(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::DemTriggerOnDtcStatus(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::DemTriggerOnDtcStatus(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::GetVin(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::GetVin(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::GetSecurityLevel(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::GetSecurityLevel(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::GetSesCtrlType(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::GetSesCtrlType(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::GetActiveProtocol(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::GetActiveProtocol(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::ResetToDefaultSession(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::ResetToDefaultSession(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::TriggerOnEvent(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::TriggerOnEvent(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::SetActiveDiagnostic(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::SetActiveDiagnostic(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::StartOfReception(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::StartOfReception(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::CopyRxData(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::CopyRxData(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::TpRxIndication(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::TpRxIndication(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::CopyTxData(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::CopyTxData(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::TpTxConfirmation(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::TpTxConfirmation(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::TxConfirmation(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::TxConfirmation(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::ComM_NoComModeEntered(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::ComM_NoComModeEntered(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::ComM_SilentComModeEntered(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::ComM_SilentComModeEntered(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::ComM_FullComModeEntered(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::ComM_FullComModeEntered(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::CallOut_ReadMemory(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::CallOut_ReadMemory(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::CallOut_WriteMemory(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::CallOut_WriteMemory(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::CallOut_SetProgConditions(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::CallOut_SetProgConditions(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::CallOut_GetProgConditions(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::CallOut_GetProgConditions(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::CallOut_ProcessRequestTransferExit(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::CallOut_ProcessRequestTransferExit(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::CallOut_ProcessRequestUpload(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::CallOut_ProcessRequestUpload(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::CallOut_ProcessRequestDownload(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::CallOut_ProcessRequestDownload(void){
 }
 
-FUNC(void, DCM_CODE) class_Dcm::CallOut_ProcessRequestFileTransfer(void){
+FUNC(void, DCM_CODE) class_Dcm_Unused::CallOut_ProcessRequestFileTransfer(void){
 }
 
 /*****************************************************/
