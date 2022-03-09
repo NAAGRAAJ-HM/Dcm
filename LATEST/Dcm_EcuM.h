15db3ea9d2234c58155fac76a60ba5131268f28b
root <nagaraja.hm.0001@gmail.com>
2022-03-09 12:54:58 +0100

    Update: submodule Dcm

--- LATEST/Dcm_EcuM.h
+++ LATEST/Dcm_EcuM.h
@@ -32,7 +32,7 @@
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-extern infEcuMClient *gptrinfEcuMClient_Dcm;
+extern infEcuMClient* gptrinfEcuMClient_Dcm;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
--- LATEST/Dcm_SchM.h
+++ LATEST/Dcm_SchM.h
@@ -32,7 +32,7 @@
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-extern infSchMClient *gptrinfSchMClient_Dcm;
+extern infSchMClient* gptrinfSchMClient_Dcm;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
