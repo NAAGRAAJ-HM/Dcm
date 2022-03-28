993bc032a90dd44b32a6202f482c963775c034dc
root <nagaraja.hm.0001@gmail.com>
2022-03-28 12:47:57 +0200

    Update: AUTOSAR basic software service module Dcm

--- LATEST/Dcm.cpp
+++ LATEST/Dcm.cpp
@@ -90,7 +90,34 @@ FUNC(void, DCM_CODE) module_Dcm::GetVersionInfo(void){
 FUNC(void, DCM_CODE) module_Dcm::MainFunction(void){
 }
 
-#include "Dcm_Unused.hpp"
+class class_Dcm_Unused{
+   public:
+      FUNC(void, DCM_CODE) DemTriggerOnDtcStatus              (void);
+      FUNC(void, DCM_CODE) GetVin                             (void);
+      FUNC(void, DCM_CODE) GetSecurityLevel                   (void);
+      FUNC(void, DCM_CODE) GetSesCtrlType                     (void);
+      FUNC(void, DCM_CODE) GetActiveProtocol                  (void);
+      FUNC(void, DCM_CODE) ResetToDefaultSession              (void);
+      FUNC(void, DCM_CODE) TriggerOnEvent                     (void);
+      FUNC(void, DCM_CODE) SetActiveDiagnostic                (void);
+      FUNC(void, DCM_CODE) StartOfReception                   (void);
+      FUNC(void, DCM_CODE) CopyRxData                         (void);
+      FUNC(void, DCM_CODE) TpRxIndication                     (void);
+      FUNC(void, DCM_CODE) CopyTxData                         (void);
+      FUNC(void, DCM_CODE) TpTxConfirmation                   (void);
+      FUNC(void, DCM_CODE) TxConfirmation                     (void);
+      FUNC(void, DCM_CODE) ComM_NoComModeEntered              (void);
+      FUNC(void, DCM_CODE) ComM_SilentComModeEntered          (void);
+      FUNC(void, DCM_CODE) ComM_FullComModeEntered            (void);
+      FUNC(void, DCM_CODE) CallOut_ReadMemory                 (void);
+      FUNC(void, DCM_CODE) CallOut_WriteMemory                (void);
+      FUNC(void, DCM_CODE) CallOut_SetProgConditions          (void);
+      FUNC(void, DCM_CODE) CallOut_GetProgConditions          (void);
+      FUNC(void, DCM_CODE) CallOut_ProcessRequestTransferExit (void);
+      FUNC(void, DCM_CODE) CallOut_ProcessRequestUpload       (void);
+      FUNC(void, DCM_CODE) CallOut_ProcessRequestDownload     (void);
+      FUNC(void, DCM_CODE) CallOut_ProcessRequestFileTransfer (void);
+};
 
 FUNC(void, DCM_CODE) class_Dcm_Unused::DemTriggerOnDtcStatus(void){
 }
