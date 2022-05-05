179c83c634ea9f13ca9de26f5eaa8d8d6c50aa27
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-05 10:09:21 +0000

    Update: AUTOSAR basic software service module Dcm

--- LATEST/Dcm.cpp
+++ LATEST/Dcm.cpp
@@ -85,12 +85,10 @@ FUNC(void, DCM_CODE) module_Dcm::InitFunction(
    if(E_OK != IsInitDone){
 #endif
       if(NULL_PTR != lptrCfgModule){
-         if(STD_LOW){
-            // check lptrCfgModule for memory faults
+         if(STD_HIGH){
             lptrCfg = lptrCfgModule;
          }
          else{
-            // use PBcfgCanIf as back-up configuration
             lptrCfg = &PBcfgDcm;
          }
       }
