2a44bfb56fa8a73c32b88dce6af49090db80554e
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-15 17:13:56 +0000

    Update: AUTOSAR basic software service module Dcm

--- LATEST/Dcm.cpp
+++ LATEST/Dcm.cpp
@@ -43,6 +43,7 @@ class module_Dcm:
 /******************************************************************************/
             Std_TypeReturn          IsInitDone{E_NOT_OK};
       const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
+            infPduRClient_Up        infPduRClient_Dcm;
 
    public:
       module_Dcm(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
