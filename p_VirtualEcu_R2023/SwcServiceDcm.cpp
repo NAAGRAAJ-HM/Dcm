6fe33a62c3dea3c543256bb51aceb250c7af2bd2
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-04-24 06:08:17 +0200

    Update: AUTOSAR service software component - diagnostic communication manager

--- p_VirtualEcu_R2023/SwcServiceDcm.cpp
+++ p_VirtualEcu_R2023/SwcServiceDcm.cpp
@@ -45,6 +45,10 @@
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
+typedef enum{
+      SwcServiceDcm_eStatusFree = 0
+   ,  SwcServiceDcm_eStatusBusy = 1
+}SwcServiceDcm_teStatus;
 
 /******************************************************************************/
 /* CONSTS                                                                     */
@@ -58,8 +62,9 @@ const CfgSwcServiceDcm_tst* SwcServiceDcm_pstConfig;
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
-LibAutosarFifo_t     SwcServiceDcm_stFifoRequestUds;
-LibAutosarFifo_tItem SwcServiceDcm_atBuffer[CfgSwcServiceDcm_dNumMaxRequests];
+LibAutosarFifo_t       SwcServiceDcm_stFifoRequestUds;
+LibAutosarFifo_tItem   SwcServiceDcm_atBuffer[CfgSwcServiceDcm_dNumMaxRequests];
+SwcServiceDcm_teStatus SwcServiceDcm_eStatus;
 
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
@@ -77,30 +82,49 @@ FUNC(void, SWCSERVICEDCM_CODE) infSwcServiceDcmSwcServiceEcuM_InitFunction(const
       ,  &SwcServiceDcm_atBuffer[0]
       ,  CfgSwcServiceDcm_dNumMaxRequests
    );
+   SwcServiceDcm_eStatus = SwcServiceDcm_eStatusFree;
    TimerP2_Init();
 }
 
 FUNC(void, SWCSERVICEDCM_CODE) infSwcServiceDcmSwcServiceEcuM_DeInitFunction(void){}
 
+void build_response(void){
+}
+
+void ProcessIfDcmIsFree(uint8 lu8IndexBufferRx){
+   SwcServiceDcm_eStatus = SwcServiceDcm_eStatusBusy;
+   TimerP2_ReStart();
+   switch( //TBD: Configurable SID table
+      McalCan_astRxFifio[lu8IndexBufferRx].McalCan_stFrameExtended.data[SwcServiceDcm_dIndexService] //TBD: Reduce complexity using unions of higher layers and Fifo APIs
+   ){
+      case 0x3E: //TBF: Implement tester present service handle here
+         build_response();
+         break;
+   }
+   SwcServiceDcm_eStatus = SwcServiceDcm_eStatusFree;
+}
+
+FUNC(void, SWCSERVICEDCM_CODE) infSwcServiceDcmSwcServicePduR_RxIndication(uint8 lu8IndexBufferRx){
+   if(
+         SwcServiceDcm_eStatusFree
+      == SwcServiceDcm_eStatus
+   ){
+      ProcessIfDcmIsFree(lu8IndexBufferRx);
+   }
+   else{
+      (void)LibAutosarFifo_Put(&SwcServiceDcm_stFifoRequestUds, lu8IndexBufferRx); //TBD: Handle not OK cases
+   }
+}
+
 FUNC(void, SWCSERVICEDCM_CODE) infSwcServiceDcmSwcServiceSchM_MainFunction(void){
    TimerP2_Runnable();
    uint8 lu8IndexBufferRx;
    while(
-         LibAutosarFifo_eStatus_OK
-      == LibAutosarFifo_Get(&SwcServiceDcm_stFifoRequestUds, &lu8IndexBufferRx)
+         LibAutosarFifo_eStatus_Underflow
+      != LibAutosarFifo_Get(&SwcServiceDcm_stFifoRequestUds, &lu8IndexBufferRx)
    ){
-      TimerP2_ReStart();
-      switch( //TBD: Configurable SID table
-         McalCan_astRxFifio[lu8IndexBufferRx].McalCan_stFrameExtended.data[SwcServiceDcm_dIndexService] //TBD: Reduce complexity using unions of higher layers and Fifo APIs
-      ){
-         case 0x3E: //TBF: Implement tester present service handle here
-            break;
-      }
+      infSwcServiceDcmSwcServicePduR_RxIndication(lu8IndexBufferRx);
    }
-}
-
-FUNC(void, SWCSERVICEDCM_CODE) infSwcServiceDcmSwcServicePduR_RxIndication(uint8 lu8IndexBufferRx){
-   (void)LibAutosarFifo_Put(&SwcServiceDcm_stFifoRequestUds, lu8IndexBufferRx); //TBD: Handle not OK cases
 }
 
 /******************************************************************************/
