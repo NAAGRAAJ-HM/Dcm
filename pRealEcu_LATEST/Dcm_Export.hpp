7faf68c6d7fa2757bc7720bce75b1042602b90d9
NagarajaHuliyapuradaMata <nagaraja.hm.0001@gmail.com>
2023-11-20 08:58:58 +0100

    AUTOSAR Migration Activities

--- pRealEcu_LATEST/Dcm_Export.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-extern FUNC(Std_ReturnType, Dcm_CODE) Dcm_GetActiveProtocol(P2VAR(Dcm_ProtocolType, AUTOMATIC, RTE_APPL_DATA) ActiveProtocol);
-extern FUNC(Std_ReturnType, Dcm_CODE) Dcm_GetSesCtrlType(P2VAR(Dcm_SesCtrlType, AUTOMATIC, RTE_APPL_DATA) SesCtrlType);
-extern FUNC(Std_ReturnType, Dcm_CODE) Dcm_GetSecurityLevel(P2VAR(Dcm_SecLevelType, AUTOMATIC, RTE_APPL_DATA) SecLevel);
-extern FUNC(Std_ReturnType, Dcm_CODE) Dcm_ResetToDefaultSession(void);
-
